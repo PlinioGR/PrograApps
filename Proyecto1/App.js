20d471dcb60da7c11061d0430b1d6e07955f4f58
PlinioGR <72715326+PlinioGR@users.noreply.github.com>
2022-09-04 17:38:00 -0500

    Add files via upload

---
+++ Proyecto1/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Plinio Alejandro Gutierrez Ramos 7E</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center' 
+  },
+  texto:{
+  color: 'white'
+  }
+});
