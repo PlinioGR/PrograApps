77fa01cf5e02a4e3c4323b669a88a0ac7c096fb0
PlinioGR <72715326+PlinioGR@users.noreply.github.com>
2022-10-06 20:59:55 -0500

    Add files via upload

---
+++ CRUD/db/firebase_config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzNAm6ICxjxxMhCIV9O4bzzGImV9fHn38",
+  authDomain: "crud-react-263f4.firebaseapp.com",
+  databaseURL: "https://crud-react-263f4-default-rtdb.firebaseio.com",
+  projectId: "crud-react-263f4",
+  storageBucket: "crud-react-263f4.appspot.com",
+  messagingSenderId: "78340730253",
+  appId: "1:78340730253:web:4a20261b438f870e40a1c4"
+};
+
+// Initialize Firebase
+/*const app = initializeApp(firebaseConfig);
+const db = getDatabase();*/
+
+
+
